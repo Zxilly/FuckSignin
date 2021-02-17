759dc94c8076edb4858c356a7bd8708614bc8da8
Zxilly <zhouxinyu1001@gmail.com>
2021-02-17 14:29:27 +0800

    fix: attempt

--- main.py
+++ main.py
@@ -3,43 +3,6 @@ import os
 
 import requests
 
-cookies = {
-    "loginName": "",
-    "loginType": "0",
-    "yzxx": ""
-}  # name学号，密码疑似统一111111
-
-signdata = {
-    "province": "",
-    "city": "",
-    "district": "",
-    "street": "",
-    "xszt": "",
-    "jkzk": "",
-    "jkzkxq": "",
-    "sfgl": "",
-    "gldd": "",
-    "mqtw": "",
-    "mqtwxq": "",
-    "zddlwz": "",
-    "sddlwz": "",
-    "bprovince": "",
-    "bcity": "",
-    "bdistrict": "",
-    "bstreet": "",
-    "sprovince": "",
-    "scity": "",
-    "sdistrict": "",
-    "lng": "",
-    "lat": "",
-    "sfby": "",
-}  # 随便填，估计提交上去也没人看
-
-formdata = {
-    "dcwj": ''
-}  # 随便填，估计提交上去也没人看
-
-
 def get_auth(
         url="https://fxgl.jx.edu.cn/4136010419/third/alipayLogin?cardId=&sfzMd5="):  # 请自行抓包补上支付宝cardid，后面那个诡异变量不会变的，我猜是身份证MD5
     main_session = requests.session()
@@ -60,6 +23,39 @@ def form(session):
 
 if __name__ == '__main__':
     if not os.getenv('CI'):
+        cookies = {
+            "loginName": "",
+            "loginType": "0",
+            "yzxx": ""
+        }  # name学号，密码疑似统一111111
+        signdata = {
+            "province": "",
+            "city": "",
+            "district": "",
+            "street": "",
+            "xszt": "",
+            "jkzk": "",
+            "jkzkxq": "",
+            "sfgl": "",
+            "gldd": "",
+            "mqtw": "",
+            "mqtwxq": "",
+            "zddlwz": "",
+            "sddlwz": "",
+            "bprovince": "",
+            "bcity": "",
+            "bdistrict": "",
+            "bstreet": "",
+            "sprovince": "",
+            "scity": "",
+            "sdistrict": "",
+            "lng": "",
+            "lat": "",
+            "sfby": "",
+        }  # 随便填，估计提交上去也没人看
+        formdata = {
+            "dcwj": ''
+        }  # 随便填，估计提交上去也没人看
         session = get_auth()
         sign(session)
         form(session)
@@ -67,7 +63,9 @@ if __name__ == '__main__':
         url = os.getenv('URL')
         cookies = json.loads(os.getenv('COOKIES'))
         signdata = json.loads(os.getenv('SIGNDATA'))
-        formdata = {"dcwj": os.getenv('FORMDATA')}
+        formdata = {
+            "dcwj": os.getenv('FORMDATA')
+        }
 
         session = get_auth(url)
         sign(session)
