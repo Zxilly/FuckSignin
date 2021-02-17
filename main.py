752582e665537ef8a2c8ef10bc8510134db75ac0
Zxilly <zhouxinyu1001@gmail.com>
2021-02-17 13:53:49 +0800

    refactor: change formdata get

--- main.py
+++ main.py
@@ -67,7 +67,7 @@ if __name__ == '__main__':
         url = os.getenv('URL')
         cookies = json.loads(os.getenv('COOKIES'))
         signdata = json.loads(os.getenv('SIGNDATA'))
-        formdata = json.loads(os.getenv('FORMDATA'))
+        formdata = {"dcwj": os.getenv('FORMDATA')}
 
         session = get_auth(url)
         sign(session)
