c7e81d7a854829e9935f23414e66ef6cdbad3efa
Zxilly <zhouxinyu1001@gmail.com>
2022-02-07 02:12:09 +0800

    fix: run webdriver under CI

--- wechat.py
+++ wechat.py
@@ -21,6 +21,12 @@ if __name__ == '__main__':
 
     options = webdriver.ChromeOptions()
     options.add_experimental_option('mobileEmulation', {'deviceName': 'iPhone X'})
+    options.add_argument("--headless")
+    options.add_argument('--no-sandbox')
+    options.add_argument('--disable-gpu')
+    options.add_argument('--disable-dev-shm-usage')
+    options.add_argument('disable-infobars')
+    options.add_argument("--disable-extensions")
     driver = webdriver.Chrome(options=options)
     driver.implicitly_wait(1)
     time.sleep(2)
